88201af9435d84ae4b369ac50d27d879cf825bec
unknown <ksh931101@naver.com>
2023-12-13 09:19:07 +0900

    basename1

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Index from "./routers/Index";
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename="/">
       <Navigation />
       <Switch>
         <Route path={"/"} exact={true} component={Home} />
