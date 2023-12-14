623430428164a9298dd21859f6d8f60ddca95d14
unknown <ksh931101@naver.com>
2023-12-14 10:32:48 +0900

    bn5

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,10 +6,10 @@ const Navigation = () => {
     return (
         <Navbar bg="primary" variant="dark">
         <Container>
-            <Navbar.Brand href="ytsMovieReact/index">Home</Navbar.Brand>
+            <Navbar.Brand href="/index">Home</Navbar.Brand>
             <Nav className="me-auto">
-            <Nav.Link href="ytsMovieReact/list">MovieList</Nav.Link>
-            <Nav.Link href="ytsMovieReact/about">About</Nav.Link>
+            <Nav.Link href="/list">MovieList</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
 
             </Nav>
         </Container>
